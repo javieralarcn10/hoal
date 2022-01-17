5990a7b5d406d7066f0b97013783087f60d38210
Javier Alarcón Sánchez <javieralarconsanchez@MacBook-Pro-de-Javier.local>
2022-01-17 20:55:23 +0100

    test 3

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ const product = require("./api/product");
 app.use(express.json({ extended: false }));
 
 app.use("/api/product", product);
-app.use("/api/increabpixel.gif", pixel);
+//app.use("/api/increabpixel.gif", pixel);
 app.use("/api/pixel", pixel);
-app.get('/increabpixel.js', function(req, res) {
-    res.sendFile(`${__dirname}/public/increabpixel.js`)
-})
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
