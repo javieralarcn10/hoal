d35f58fec764a24e6ecb42cee6d4b98cf9d94102
Javier Alarcón Sánchez <javieralarconsanchez@MacBook-Pro-de-Javier.local>
2022-01-17 21:00:10 +0100

    gitignore

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ const pixel = require("./api/pixel");
 app.use(express.json({ extended: false }));
 
 app.use("/api/product", product);
-app.use("/api/increabpixel.gif", pixel);
+//app.use("/api/increabpixel.gif", pixel);
 app.use("/api/pixel", pixel);
-app.get('/increabpixel.js', function(req, res) {
-    res.sendFile(`${__dirname}/public/increabpixel.js`)
-})
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
